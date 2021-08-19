fdf372a95576b5416382671940fa00f8e7b2d2d2
leeharyun <gkfbs3399@naver.com>
2021-08-20 07:17:37 +0900

    key 수정

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
 // unsplash API
 export let unsplashAPIConfig = {
     key: {
-        accessKey: 'NqbRGEoBtYi-Ns5hhhkdMfu_3Vf9X6OLfvZ8WcQrtIs',
-        secretKey: '7Nv8ZcHRdvBXL5nsrkX26k3u8zrgzxaRBO8srU9bXaE'
+        accessKey: 'YOUR ACCESS KEY',
+        secretKey: 'YOUR SECRET KEY'
     },
     redirect_uri: 'http://localhost/auth',
     response_type: 'code',
     scope: 'public+write_likes',
     grant_type: 'authorization_code',
-    Authorization: 'Client-ID mv84kleu5X5Azwyve5tTjszOftohylKesJ7brt2W3T4'
 };
